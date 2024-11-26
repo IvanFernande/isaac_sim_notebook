b1e30a4ad9b6b749cb41e1063e0d6eb7f4c41e74
Iván Fernández Gómez <149154386+IvanFernande@users.noreply.github.com>
2024-11-26 13:50:44 +0100

    move_joints.py

---
+++ scripts/move_joints.py
@@ -0,0 +1,15 @@
+import omni
+from omni.isaac.dynamic_control import _dynamic_control
+import numpy as np
+
+omni.timeline.get_timeline_interface().play()
+dc = _dynamic_control.acquire_dynamic_control_interface()
+articulation = dc.get_articulation("/World/kuka_lwr/world")
+dof_count = dc.get_articulation_dof_count(articulation)
+print(dof_count)
+dc.wake_up_articulation(articulation)
+joint_angles = [np.array([0.0, 0.7, 0.0, 0.0, -0.0, 0.0, 1.4])]
+print(joint_angles)
+dc.set_articulation_dof_position_targets(articulation, joint_angles)
+joint_state = dc.get_articulation_dof_states(articulation, _dynamic_control.STATE_POS)
+print(joint_state)
