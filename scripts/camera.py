7a977ab6b77a1b5f0ad607b44ec63b3927c507af
Iván Fernández Gómez <149154386+IvanFernande@users.noreply.github.com>
2024-11-26 16:35:20 +0100

    Update camera.py

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import os
 import omni
 from omni.isaac.core import World
 from omni.isaac.core.objects import DynamicCuboid
@@ -27,4 +28,7 @@ camera.initialize()
 rgb_image = camera.get_rgb()
 print(rgb_image)
 print(f"Forma de la imagen RGB: {rgb_image.shape}")
-cv2.imshow("Imagen de la Cámara", rgb_image)
+
+ruta_guardado = '/home/javier/RL_docs/kuka_lwr_isaac_sim/kuka_lwr_isaac_project/img'
+os.chdir(ruta_guardado)
+cv2.imwrite('imagen_capturada.png', rgb_image)
